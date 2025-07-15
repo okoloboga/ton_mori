664ddda1b6c1e85d9caef3c18d4a9fbc63ba92c6
okoloboga <okoloboga37@gmail.com>
2025-07-15 15:50:47 +0300

    node packages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+   import ReactDOM from 'react-dom/client';
+   import App from './App.jsx';
+   import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+   // Инициализация Telegram WebApp
+   if (window.Telegram?.WebApp) {
+     window.Telegram.WebApp.ready();
+     window.Telegram.WebApp.expand();
+     console.log('Telegram WebApp initialized:', window.Telegram.WebApp.initData);
+   } else {
+     console.log('Telegram WebApp not available');
+   }
+
+   ReactDOM.createRoot(document.getElementById('root')).render(
+     <React.StrictMode>
+       <App />
+     </React.StrictMode>
+   );
